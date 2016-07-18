4024180c497d37724c66f62bb55ff9b82cc2950c
Dmytro <dimagit24@gmail.com>
2016-07-18 12:26:10 +0300

    added modular structure

---
+++ rout/src/main/java/com/parkhomenko/rout/Run.java
@@ -0,0 +1,15 @@
+package com.parkhomenko.rout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dmytro on 18.07.16.
+ */
+
+@SpringBootApplication
+public class Run {
+    public static void main(String[] args) {
+        SpringApplication.run(Run.class, args);
+    }
+}
