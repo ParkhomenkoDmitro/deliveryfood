38564af7ed3bdcce7711341b117800dafb3adaff
Dmytro <dimagit24@gmail.com>
2016-08-01 13:16:10 +0300

    added custom serializer for user persistence class

---
+++ rout/src/main/java/com/parkhomenko/rout/config/JsonConfig.java
@@ -0,0 +1,25 @@
+package com.parkhomenko.rout.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.parkhomenko.common.domain.User;
+import com.parkhomenko.rout.serializer.UserSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * Created by dmytro on 01.08.16.
+ */
+@Configuration
+public class JsonConfig {
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(User.class, new UserSerializer(User.class));
+        mapper.registerModule(module);
+        return mapper;
+    }
+}
---
+++ rout/src/main/java/com/parkhomenko/rout/serializer/UserSerializer.java
@@ -0,0 +1,26 @@
+package com.parkhomenko.rout.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import com.parkhomenko.common.domain.User;
+
+import java.io.IOException;
+
+/**
+ * Created by dmytro on 01.08.16.
+ */
+public class UserSerializer extends StdSerializer<User> {
+
+    public UserSerializer(Class<User> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(User user, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeStartObject();
+        jsonGenerator.writeNumberField("id", user.getId());
+        jsonGenerator.writeStringField("name", user.getName());
+        jsonGenerator.writeEndObject();
+    }
+}
